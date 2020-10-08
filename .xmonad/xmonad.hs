62f89fc2e2abaf98df0f61fa2561c658901fad84
Jan Schmitt <jan@smittie.de>
2020-10-08 20:52:14 +0200

    config: tmux and ideavim

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,20 @@
+-- default desktop configuration for Fedora
+
+import System.Posix.Env (getEnv)
+import Data.Maybe (maybe)
+
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Config.Gnome
+import XMonad.Config.Kde
+import XMonad.Config.Xfce
+
+main = do
+     session <- getEnv "DESKTOP_SESSION"
+     xmonad  $ maybe desktopConfig desktop session
+
+desktop "gnome" = gnomeConfig
+desktop "kde" = kde4Config
+desktop "xfce" = xfceConfig
+desktop "xmonad-mate" = gnomeConfig
+desktop _ = desktopConfig
