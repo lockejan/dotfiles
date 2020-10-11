4e3e288678cbf3712d90c4a251ebfeb272072bcb
Jan Schmitt <jan@smittie.de>
2020-10-11 15:57:33 +0200

    setup: basic xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,17 +4,30 @@ import System.Posix.Env (getEnv)
 import Data.Maybe (maybe)
 
 import XMonad
+import XMonad.Hooks.DynamicLog
 import XMonad.Config.Desktop
 import XMonad.Config.Gnome
 import XMonad.Config.Kde
 import XMonad.Config.Xfce
 
-main = do
-     session <- getEnv "DESKTOP_SESSION"
-     xmonad  $ maybe desktopConfig desktop session
+-- main = do
+--      session <- getEnv "DESKTOP_SESSION"
+--      xmonad  $ maybe desktopConfig desktop session
 
-desktop "gnome" = gnomeConfig
-desktop "kde" = kde4Config
-desktop "xfce" = xfceConfig
-desktop "xmonad-mate" = gnomeConfig
-desktop _ = desktopConfig
+-- desktop "gnome" = gnomeConfig
+-- desktop "kde" = kde4Config
+-- desktop "xfce" = xfceConfig
+-- desktop "xmonad-mate" = gnomeConfig
+-- desktop _ = desktopConfig
+main = xmonad =<< xmobar def
+                {
+                    modMask = mod4Mask,
+                    terminal    = "alacritty",
+                    startupHook = myStartupHook
+                }
+
+myStartupHook = spawn "picom"
+                >> spawn "feh --bg-fill /home/schmitt/Pictures/cheng-xiao-leaf.jpg"
+                >> spawn "dunst"
+                >> spawn "autorandr --change"
+                >> spawn "xsetroot -cursor_name left_ptr"
